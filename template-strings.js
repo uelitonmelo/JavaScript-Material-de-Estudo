e423155d2ad467e820406f4f4ab77761b6d45ccf
Uéliton <103962324+uelitonmelo@users.noreply.github.com>
2022-12-03 23:41:06 -0400

    Add files via upload

---
+++ template-strings.js
@@ -0,0 +1,11 @@
+const nome = "Uéliton";
+const idade = 2022-1987;
+const cidadeDeNascimento = "Arap";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
+
+
